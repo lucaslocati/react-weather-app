41daeecd129386c6739b8c04021dc349c6b3de1c
Lucas Locati <lucas.box@hotmail.com>
2022-07-17 19:24:28 -0300

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0ef1de861fmshcf829a2261f9855p16a01ejsnfd5c9a9a5965',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL =  "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "fa5c27a735aed2f1283e89b24ec55496";
